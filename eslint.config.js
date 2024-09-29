ec310a7b6a0a24e40a59b04b786ae3a7f797c9cf
Elian Batista <eliabatista2000@gmail.com>
2024-09-29 20:05:20 -0300

    chore: add vue/require-prop-types rule off

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,9 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 export default [
   ...pluginVue.configs['flat/recommended'],
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'vue/require-prop-types': 'off',
+    },
+  },
 ]
