fb50d82718ffa48e122c0d2eb3ac47fa9ad12302
Elian Batista <eliabatista2000@gmail.com>
2024-09-29 21:57:52 -0300

    feat: create InputError

---
+++ src/components/InputError/InputError.vue
@@ -0,0 +1,22 @@
+<script setup>
+  defineProps({
+    error: {
+      type: String,
+      default: '',
+    },
+  })
+</script>
+
+<template>
+  <div :class="$style.error">
+    {{ error }}
+  </div>
+</template>
+
+<style lang="scss" module>
+  .error {
+    color: var(--error);
+    font-size: 14px;
+    margin-top: 4px;
+  }
+</style>
