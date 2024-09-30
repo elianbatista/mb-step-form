e97d0ab9952604ab58541a4f9505959d69425517
Elian Batista <eliabatista2000@gmail.com>
2024-09-30 09:15:30 -0300

    feat: add new validation methods in useValidation

--- src/composables/useValidation.js
+++ src/composables/useValidation.js
@@ -27,9 +27,80 @@ export const useValidation = () => {
     return ''
   }
 
+  const validateCpf = (cpf) => {
+    cpf = cpf.replace(/[^\d]+/g, '')
+
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+      return 'CPF Inválido'
+    }
+
+    let sum = 0
+    for (let i = 0; i < 9; i++) {
+      sum += parseInt(cpf[i]) * (10 - i)
+    }
+    let firstVerifier = 11 - (sum % 11)
+    firstVerifier = firstVerifier >= 10 ? 0 : firstVerifier
+
+    sum = 0
+    for (let i = 0; i < 10; i++) {
+      sum += parseInt(cpf[i]) * (11 - i)
+    }
+    let secondVerifier = 11 - (sum % 11)
+    secondVerifier = secondVerifier >= 10 ? 0 : secondVerifier
+
+    if (
+      firstVerifier !== parseInt(cpf[9]) ||
+      secondVerifier !== parseInt(cpf[10])
+    ) {
+      return 'CPF Inválido'
+    }
+
+    return ''
+  }
+
+  const validateCnpj = (cnpj) => {
+    cnpj = cnpj.replace(/\D/g, '')
+
+    if (cnpj.length !== 14) return 'CNPJ Inválido'
+
+    return ''
+  }
+
+  const validateDate = (dateString) => {
+    const regex = /^\d{4}-\d{2}-\d{2}$/
+    if (!regex.test(dateString)) {
+      return 'Data Inválida'
+    }
+
+    const [year, month, day] = dateString.split('-').map(Number)
+
+    const date = new Date(year, month - 1, day)
+    if (
+      date.getFullYear() !== year ||
+      date.getMonth() !== month - 1 ||
+      date.getDate() !== day
+    ) {
+      return 'Data Inválida'
+    }
+
+    return ''
+  }
+
+  const validateMinLength = (string, length) => {
+    if (string.length < length) {
+      return `O campo tem que ter no mínimo ${length} caracteres`
+    }
+
+    return ''
+  }
+
   return {
     validator,
     validateEmail,
     validateRequired,
+    validateCpf,
+    validateDate,
+    validateMinLength,
+    validateCnpj,
   }
 }
