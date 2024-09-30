62580322fecafe1733d88de780b39bbb7c2b748e
Elian Batista <eliabatista2000@gmail.com>
2024-09-29 21:24:18 -0300

    feat: create BaseButton

---
+++ src/components/BaseButton/BaseButton.vue
@@ -0,0 +1,46 @@
+<script setup>
+  defineProps({
+    type: {
+      type: String,
+      default: 'button',
+      validator: (value) => {
+        return ['button', 'submit'].includes(value)
+      },
+    },
+    design: {
+      type: String,
+      default: 'solid',
+      validator: (value) => {
+        return ['solid', 'outlined'].includes(value)
+      },
+    },
+  })
+</script>
+
+<template>
+  <button
+    :class="[$style.button, design === 'outlined' && $style.outlined]"
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" module>
+  .button {
+    background-color: var(--primary-color);
+    border: none;
+    width: 100%;
+    padding: 10px;
+    border-radius: 4px;
+    color: #fff;
+    font-size: 16px;
+    border: 1px solid var(--primary-color);
+    cursor: pointer;
+
+    &.outlined {
+      background-color: #fff;
+      color: var(--primary-color);
+    }
+  }
+</style>
